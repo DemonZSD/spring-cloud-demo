7591707f11cbd66a6cef7eaf17253cc3545b70a7
DemonZSD <498537806@qq.com>
2019-09-26 20:07:31 +0800

    modify module name to spring-cloud-eureka

--- src/main/java/com/weshzhu/springcloud/SpringCloudApplication.java
+++ src/main/java/com/weshzhu/springcloud/SpringCloudApplication.java
@@ -2,6 +2,8 @@ package com.weshzhu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 @EnableEurekaServer
 @SpringBootApplication
 public class SpringCloudApplication {
